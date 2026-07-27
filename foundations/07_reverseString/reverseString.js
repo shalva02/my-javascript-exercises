ba688ecc6aa870218278b6dd161afa7febb8e354
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-07-28 01:16:17 +0400

    finish exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  return str.split("").reverse().join("");
 };
-
+reverseString("hello");
 // Do not edit below this line
 module.exports = reverseString;
