2e2847da56f0b93bde726a0fc3fd6560176bf981
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-07-28 01:05:47 +0400

    finish exrecise 06

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (str, num) {
+  let string = "";
+  if (num < 0) return "ERROR";
+  for (i = 0; i < num; i++) {
+    string += str;
+  }
+  return string;
 };
+repeatString("hay", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
