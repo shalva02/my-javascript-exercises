dc4f18f83aa8d54edeaf68edddfe38c5a2711178
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-07-28 03:53:02 +0400

    finish exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (f, l) {
+  let result = 0;
+  if (f < 0 || l < 0 || !Number.isInteger(f) || !Number.isInteger(l)) {
+    return "ERROR";
+  }
+  let arr = Array.from(arguments).sort((a, b) => a - b);
+  for (i = arr[0]; i <= arr[1]; i++) {
+    result += i;
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
