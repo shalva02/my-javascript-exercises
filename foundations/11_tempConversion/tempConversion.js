b6a7c98bf61fb7f3d53f03d3d1b26c64f2161d2e
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-07-30 02:01:10 +0400

    finish exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (num) {
+  let result = ((num - 32) * 5) / 9;
+  return +result.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (num) {
+  let result = num * 1.8 + 32;
+  return +result.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
