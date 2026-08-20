ec73055918d780fba97b69d4fdae0e77b486729b
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-08-21 01:19:13 +0400

    finish exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  const alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
+  const string = str
+    .toLowerCase()
+    .split("")
+    .filter((item) => alpha.includes(item))
+    .join("");
+  const reversedString = string.split("").reverse().join("");
+  return string === reversedString;
 };
 
 // Do not edit below this line
