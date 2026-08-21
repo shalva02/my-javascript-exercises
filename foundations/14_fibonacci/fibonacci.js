f0519761f71a2c57099cac968e99bf09b9540c3a
shalva02 <44312272+shalva02@users.noreply.github.com>
2026-08-22 01:52:18 +0400

    finish exercises

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+  num = +num;
+  let current = 1;
+  let prevOne = 1;
+  let prevTwo = 0;
+  if (num < 0) return "OOPS";
+  if (num === 0) return 0;
+  for (let i = 2; i <= num; i++) {
+    current = prevOne + prevTwo;
+    prevTwo = prevOne;
+    prevOne = current;
+  }
+  return current;
 };
 
 // Do not edit below this line
